c8b77592e4174ba96601a9c3d0f2db3ec40629da
Denys <drtymkivstain@gmail.com>
2022-09-29 02:34:08 +0300

    some fix

--- src/main/java/com/example/startrest/Library.java
+++ src/main/java/com/example/startrest/Library.java
@@ -3,20 +3,41 @@ package com.example.startrest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import javax.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 @Component
 public class Library {
     private List<Book> books;
 
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+    public void addBook(Book book) {
+        books.add(book);
+    }
+    public Book getBookByID(Long id) {
+        for(Book book : books) {
+            if(book.getId().equals(id)) {
+                return book;
+            }
+        }
+        throw new NoSuchElementException("No book with this ID");
+    }
+    public void addBookByIndex(Book book, int index) {
+        books.add(index,book);
+    }
+
     public Library() {
        books = new ArrayList<>();
-        System.out.println("suchesfull");
     }
 
     public List<Book> getBooks() {
         return books;
     }
+
 }
--- src/main/java/com/example/startrest/myConfig.java
+++ src/main/java/com/example/startrest/myConfig.java
@@ -1,5 +1,6 @@
 package com.example.startrest;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
